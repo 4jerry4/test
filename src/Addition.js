abe909dcf319b667771a1cbb8b3829ee74859e5b
4jerry4 <paramrajput1999@gmail.com>
2019-08-08 22:31:48 +0530

    changing content from addition.js

--- src/Addition.js
+++ src/Addition.js
@@ -4,7 +4,7 @@ const Addition = () =>{
 	return (
 		<div>
 			<p className = 'f3 dim underline pa3 pointer'> 
-				<a href="https://gouravkashiv.github.io/project404" className = 'black '> About 4 Of Us </a>
+				<a href=" https://4jerry4.github.io/project404/" className = 'black '> About 5 Of Us </a>
 			</p>
 		</div>
 )};
